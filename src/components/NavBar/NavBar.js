238bea309427f75817e758181d3786b296cd365f
Bruno Borelli <brunoborellipro@gmail.com>
2021-07-20 10:46:28 -0300

    NavBar \o/

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,42 +1,37 @@
-import React,{Fragment, useState} from 'react'
+import React, { Fragment, useState } from 'react'
 import Logo from '../../img/logo.png'
 import Navbar from 'react-bootstrap/Navbar'
 import NavDropdown from 'react-bootstrap/NavDropdown'
 import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 import './NavBar.css'
 
-const NavBarSite = ()=>{
-    return(
+const NavBarSite = () => {
+    return (
         <Fragment className="navbar">
-<Navbar bg="light" expand="lg">
-            <Container>
-                <Navbar.Brand href="/"><img className="logo_img" src={Logo}></img></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto ">
-                    <div className="nav_container">
-                    <Nav.Link href="#home">
-                        <div className="titulo">
-                            <h5>Imobiliária</h5>
-                            <h5>Companhia de Oportunidades</h5>
-                        </div>
-                    </Nav.Link>
-                    <div className="nav_links">
-                        <Nav.Link href="#link">Início</Nav.Link>
-                        <Nav.Link href="#link">Comprar</Nav.Link>
-                        <Nav.Link href="#link">Alugar</Nav.Link>
-                        <Nav.Link href="#link">Vender</Nav.Link>
-                    </div>
-                    </div>
-                    
-                    
-                </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+            <Navbar bg="light" expand="md">
+                <Container>
+                    <Navbar.Brand href="/"><img className="logo_img" src={Logo}></img></Navbar.Brand>
+                    <Navbar.Brand href="/"><div className="titulo">
+                        <h5>Imobiliária</h5>
+                        <h5>Companhia de Oportunidades</h5>
+                    </div></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                        <Nav className="justify-content-end">
+                            <Nav.Link className="nav_link" href="#link">Início</Nav.Link>
+                            <Nav.Link className="nav_link" href="#link">Comprar</Nav.Link>
+                            <Nav.Link className="nav_link" href="#link">Alugar</Nav.Link>
+                            <Nav.Link className="nav_link" href="#link">Vender</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+
+                </Container>
+            </Navbar>
         </Fragment>
-        
+
     );
 }
 
