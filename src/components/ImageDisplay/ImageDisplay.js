580ecf719f9a229a3b9583dbcc187c7eceadd096
Daniel Sávio <spnjc137@gmail.com>
2021-09-18 22:58:45 -0300

    Arrumando o css e passando o nome da pasta da foto clicada para a url

--- src/components/ImageDisplay/ImageDisplay.js
+++ src/components/ImageDisplay/ImageDisplay.js
@@ -1,15 +1,25 @@
-import React from 'react'
-import urbano1 from '../../img/urbano/urbano1.png'
-import './ImageDisplay.css'
-import Card from 'react-bootstrap/Card'
+import React from 'react';
+import urbano1 from '../../img/urbano/urbano1.png';
+import './ImageDisplay.css';
+import Card from 'react-bootstrap/Card';
+import { useHistory } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 
-const ImageDisplay = ({imagem, complemento, desc, cidade, bairro})=>{
+const ImageDisplay = ({ imagem, complemento, desc, cidade, bairro }) => {
     const url = require(`../../${imagem}`)
     console.log(url)
     console.log(urbano1)
-    return(
-        <Card className="imovel" style={{ width: '18rem' }}>
+    const history = useHistory();
+
+    function handleHistory(e) {
+        console.log(imagem.split("/", 3)[2]); //aqui deve me passar o nome da pasta do imóvel
+        let pastaImovel = imagem.split("/", 3)[2]
+        history.push(`contato/${pastaImovel}`);
+
+    }
+
+    return (
+        <Card className="imovel" style={{ width: '18rem' }} onClick={(e) => handleHistory(e)} >
             <Card.Img className="imovel_img" variant="top" src={url} />
             <Card.Body>
                 <Card.Title>{complemento}</Card.Title>
